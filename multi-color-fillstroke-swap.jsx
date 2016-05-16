d8548db0e78fae6a1978410595fd22898b9efc72
koloskus <jack.koloskus@gmail.com>
2016-05-16 16:04:35 -0600

    Error handling + more flexibility
    Made the swap a little more comprehensive. Now if you select one object that has a fill or stroke that is set to [None] as well as a shape that has both a fill and a stroke this will still work.
    
    Script also now retains stroke weight.

--- multi-color-fillstroke-swap.jsx
+++ multi-color-fillstroke-swap.jsx
@@ -16,12 +16,63 @@
 
 var docRef = app.activeDocument;
 var selRef = docRef.selection;
-var colorHolder = new RGBColor();
+
+if (selRef.length == 0) {alert("You haven't selected anything, dummy.")}
 
 for (var i=0; i<selRef.length; i++) {
-	var pathRef = selRef[i];
-	colorHolder = pathRef.fillColor;
-	pathRef.filled = false;
+	var pathRef = selRef[i];	
+	
+	if 	(!(pathRef.fillColor instanceof RGBColor) && 
+		(!(pathRef.fillColor instanceof GradientColor)) && 
+		(!(pathRef.fillColor instanceof NoColor)) && 
+		(!(pathRef.fillColor instanceof GrayColor))) {
+			alert("No fill color data found.")
+	}
+	
+	if 	(!(pathRef.strokeColor instanceof RGBColor) && 
+		(!(pathRef.strokeColor instanceof GradientColor)) && 
+		(!(pathRef.strokeColor instanceof NoColor)) && 
+		(!(pathRef.strokeColor instanceof GrayColor))) {
+			alert("No stroke color data found.")
+	}
+	
+	var strokeWidthHolder = pathRef.strokeWidth;
+	var fillColorHolder = pathRef.fillColor;
+	var strokeColorHolder = pathRef.strokeColor;
+	
+	pathRef.stroked = false;
 	pathRef.stroked = true;
-	pathRef.strokeColor = colorHolder;
+	
+	if (pathRef.fillColor instanceof RGBColor) {
+		pathRef.strokeColor = new RGBColor()
+	} else if (pathRef.fillColor instanceof GradientColor) {
+		pathRef.strokeColor = new GradientColor()
+	} else if (pathRef.fillColor instanceof NoColor) {
+		strokeWidthHolder = 0;
+		pathRef.strokeColor = new NoColor()
+	} else if (pathRef.fillColor instanceof GrayColor) {
+		strokeWidthHolder = 0;
+		pathRef.strokeColor = new GrayColor()
+	}
+	
+	pathRef.strokeColor = fillColorHolder;
+	pathRef.strokeWidth = strokeWidthHolder;
+	
+	
+	
+	pathRef.filled = false;
+	pathRef.filled = true;
+	
+	if (pathRef.strokeColor instanceof RGBColor) {
+		pathRef.fillColor = new RGBColor()
+	} else if (pathRef.strokeColor instanceof GradientColor) {
+		pathRef.fillColor = new GradientColor()
+	} else if (pathRef.strokeColor instanceof NoColor) {
+		pathRef.fillColor = new NoColor()
+	} else if (pathRef.strokeColor instanceof GrayColor) {
+		pathRef.fillColor = new GrayColor()
+	}
+	
+	pathRef.fillColor = strokeColorHolder;
+	
 }
